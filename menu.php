2c3912059717d0093deba13876274e1a7b7e8215
Michael Torres <maicon_towers@hotmail.com>
2014-08-23 13:10:05 -0300

    Criando o menu

---
+++ menu.php
@@ -0,0 +1,8 @@
+﻿
+<ul class="nav nav-pills">
+  <li><a href="index.php?p=H" class="active">Home</a></li>
+  <li><a href="index.php?p=E">Empresa</a></li>
+  <li><a href="index.php?p=P">Produtos</a></li>
+  <li><a href="index.php?p=S">Serviços</a></li>
+  <li><a href="index.php?p=C">Contato</a></li>
+</ul>

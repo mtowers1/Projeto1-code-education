3e3214b9dc8e6f4ce8a78e6e1d2a1d98fa096cb3
Michael Torres <maicon_towers@hotmail.com>
2014-08-23 13:12:06 -0300

    Criando o topo do site

---
+++ topo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="utf-8">
+<meta name="author" content="Michael Torres de Souza">
+<meta name="description" content="Projeto Code Education - Bootstrap">
+<meta name="keywords" content="code education">
+<title>Projeto 1</title>
+<link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">

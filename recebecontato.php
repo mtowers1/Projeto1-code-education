b3a3cdb27d74d0d1c3d4f2d8060a483a022c36a3
Michael Torres <maicon_towers@hotmail.com>
2014-08-26 20:26:10 -0300

    Correção do nome do arquivo para corrigir sua rota

---
+++ recebecontato.php
@@ -0,0 +1,5 @@
+<div class="alert alert-success">Dados enviados com sucesso, abaixo seguem os dados que você enviou</div><br /><br />
+<p class="text-info">Nome: <?php echo $_POST['inputNome'];?> </p>
+<p class="text-info">E-mail: <?php echo $_POST['inputEmail'];?> </p>
+<p class="text-info">Assunto: <?php echo $_POST['inputAssunto'];?> </p>
+<p class="text-info">Mensagem: <?php echo $_POST['inputMensagem'];?> </p>

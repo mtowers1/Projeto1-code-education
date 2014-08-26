66b09ef7c05cb1b6b5a881206d6cbda9c803d514
Michael Torres <maicon_towers@hotmail.com>
2014-08-26 20:27:44 -0300

    Criando função e melhorando a validação das rotas

--- validacao.php
+++ validacao.php
@@ -1,6 +1,34 @@
 <?php
-$menu= array('H','E','P','S','C','R');
+function VerificaRotas(){
+	$rotas=array('home','empresa','produtos','servicos','contato','recebecontato');
+	$rota= parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+	$path = explode("/",$rota['path']);
+	$arquivo=strtolower($path[1]);
 
-if(in_array($_GET['p'],$menu)==false && $_GET['p']!=""){
-	exit(utf8_decode('Página não identificada!'));
+	if($arquivo==""){
+	$arquivo="home";
+	}
+	
+	if(in_array($arquivo,$rotas)){
+			if (!file_exists($arquivo.".php")) {
+				echo utf8_decode("Página não encontrada!");
+				http_response_code(404);
+				exit();
+			}
+			return $arquivo;
+	}
+	else{
+				echo utf8_decode("Página não encontrada!");
+				http_response_code(404);
+				exit();
+		}
+	
+	
+	
+
 }
+
+$arquivo=VerificaRotas();
+
+
+
